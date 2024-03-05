e6c4de72917fadce26c5db918e3cfd97b9f654da
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-05 19:54:30 +0500

    HTML

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// javascript learning 
+
+// it is interpreted language  
+// language pf browser
+// language of front end
+// for run time we need node js
+// we can use different functionality 
+// phyton c sharp , java , c  (these language are sever side  they dont have  like slide show etc for cascading sheet )
+// we can also  make interactive interfaces by using very powerful frmamework  like react ,anguler,viewjs
+// we can also make full satck applictaion by using node js 
+// used in mob dev like react native electron js etc  
