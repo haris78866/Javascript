156ef46632092da6efe7b880b27e57a32f256afb
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-08 01:59:23 +0500

    changes

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-alert('hello world ');
-alert('java script');
+//alert('hello world ');
+//alert('java script');
+
+console.log('Hello world');
+// we can add diffferent things to our module 
+console.warn('This is an error');
+
