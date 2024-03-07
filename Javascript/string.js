5f93f605f66935ebc99be3d58f679f593ced5011
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-08 02:30:48 +0500

    changes

--- Javascript/string.js
+++ Javascript/string.js
@@ -4,4 +4,9 @@ const age=20;
 
 // to output the name and age 
 console.log('MY name is ',name,'my age is ',age);
+ 
 
+// tempelate 
+const hello=('MY name is ',name,'my age is ',age);
+
+console.log(hello);
