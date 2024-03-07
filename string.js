b513266dc0c7f127db47d2b5a42f6cdac1bb701f
Haris <144903716+haris78866@users.noreply.github.com>
2024-03-08 02:28:49 +0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,12 @@
+// string 
+const name='Haris';
+const age=20;
+
+// to output the name and age 
+console.log('MY name is ',name,'my age is ',age);
+ 
+
+// tempelate 
+const hello=('MY name is ',name,'my age is ',age);
+
+console.log(hello);
