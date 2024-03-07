314ddd926a5635e84d82bd2c76d8b03680e9cd77
Haris <144903716+haris78866@users.noreply.github.com>
2024-03-08 02:29:34 +0500

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,11 @@
+// stirng , numbers, boolean , nulll,undefined
+
+const name ='Haris'; 
+const age =30;
+const iscool=true;
+const rating =4.5; // there is no float datatype in javascript it is just act as data
+
+// console log is used for to show output
+console.log(typeof iscool);
+console.log(typeof  rating);
+
