b75d70a1cc71c29337b5c38be7b98ddcfea265fc
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 01:51:52 +0500

    Changes

--- objectarray.js
+++ objectarray.js
@@ -19,5 +19,18 @@ const todos = [
   
   console.log(todos);
 
+// if we just want to print the text or id  property 
+console.log(todos[1].id);
+console.log(todos[1].text);
 
-  
+// json is  data formta for full satck development  ,APIs project
+// we we send something to data server then we use json format
+// it is commonly used for data exchange between a server and a webpage 
+
+
+
+
+// if we need a JSON string 
+const todoJSON =JSON.stringify(todos);
+
+console.log(todoJSON);
