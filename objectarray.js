9735d3043297d05af5175577fff654b8f703ae8b
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 01:40:16 +0500

    Changes

---
+++ objectarray.js
@@ -0,0 +1,18 @@
+// Array of objects
+const todos = [
+    {
+      id: 1,
+      text: 'Take out trash',
+    },
+    {
+      id: 2,
+      text: 'Take out trash',
+    },
+    {
+      id: 3,
+      text: 'Take out trash',
+    },
+  ];
+  
+  console.log(todos);
+  
