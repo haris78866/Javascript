7c6a86f7373de22976014585b79366b13d6b3b43
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 01:28:01 +0500

    Changes

--- object.js
+++ object.js
@@ -15,3 +15,16 @@ const person = {
 }
 
 console.log(person);
+
+
+// if we meed only hoobies   from the person clases
+console.log(person.hobbies);
+
+// if we need only city or name  from the peroson clases 
+console.log(person.firstname);
+console.log(person.address.city );
+
+// we can also add something to the class by using this 
+person.email='haris151@gmail.com';
+console.log(person);
+
