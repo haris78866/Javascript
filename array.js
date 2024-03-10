6bc9079a0691d343f2bc35b57ff9f1e1691c70de
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 00:54:30 +0500

    Changes

--- array.js
+++ array.js
@@ -1,4 +1,24 @@
 // array in Java script 
 // array are the variable that hold multiple variable
 
+const number =[12,3,4,5,5];
 
+console.log(number);
+
+const fruits=['apple','banana','grapes'];
+
+console.log(fruits);
+
+// arrary is 0 based
+// try to used const with an array other wise we will get an array
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// we can also add by using index of array 
+fruits[3]='grapes';
+
+
+fruits.push['mangoes'];
+
+console.log(fruits);
