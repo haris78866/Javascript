f41bbaa1197e3d4cbb1442bdbb0cb73a67bf0a7d
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 01:19:08 +0500

    Changes

---
+++ object.js
@@ -0,0 +1,17 @@
+// object literals
+
+const person = {
+    firstname: 'haris',
+    lastname: 'shahzad',
+    age : 30,
+    hobbies:['fottbal','games'],
+    address:   {
+        street: 7,
+        city : 'pwr',
+
+
+    }
+
+}
+
+console.log(person);
