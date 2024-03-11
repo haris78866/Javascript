0f9916ba4c21e967766ba7d5eb55fcaa586b0ef5
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 13:25:32 +0500

    Changes

---
+++ switch.js
@@ -0,0 +1,49 @@
+// switch conditions 
+
+
+
+const x=prompt('Enter any number :');
+
+switch(parseInt(x)) {
+    case 1 : 
+    console.log('Hello');
+    break;
+    case 2 : 
+    console.log('Hello');
+    break;
+    case 3 : 
+    console.log('Hello');
+    break;
+    case 4 : 
+    console.log('Hello');
+    break;
+    case 5 : 
+    console.log('Hello');
+    break;
+    case 6 : 
+    console.log('Hello');
+    break;
+    case 7 : 
+    console.log('Hello');
+    break;
+    case 8 : 
+    console.log('Hello');
+    break;
+    case 9 : 
+    console.log('Hello');
+    break;
+    case 10 : 
+    console.log('Hello');
+    break;
+    case 11 : 
+    console.log('Hello');
+    break;
+    case 12 : 
+    console.log('Hello');
+    break;
+default :
+     console.log('You entered invalid  number :');
+    
+
+}
+
