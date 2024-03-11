4904bf50549bb5f009f7f3da4aa455191e004747
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 13:12:03 +0500

    Changes

--- condition.js
+++ condition.js
@@ -1,9 +1,9 @@
 // condition in the JS
 
 // if 
-const x=1;
-
-if(x==10){
+const x=11;
+// we can also use  === to chxeck the types
+if(x===10){
   console.log(true);
 }
 
@@ -15,3 +15,29 @@ if(x==10){
 else {
     console.log(false);
 }
+
+
+// else if 
+
+if(x==10){
+    console.log(true);
+  }
+  else if(x>10){
+      console.log('x > 10');
+  }
+  else if(x<10){
+    console.log('x < 10');
+}
+  else {
+    console.log(false);
+  }
+
+  //we can also and &&  or || operter
+
+  // ternary operater 
+
+  const y=11;
+
+  const color= y>10 ? 'red' : 'blue';
+
+  console.log(color);
