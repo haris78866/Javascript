e10fd5b8ebf287e4a19b3b83688a7d17dcb65cec
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 16:50:16 +0500

    changes

---
+++ funprototype.js
@@ -0,0 +1,21 @@
+// function prototypes
+
+// class
+
+class  person {
+// default constructer
+perosn(){
+
+}
+//  paramterized constructer
+ perosn (firstname,lastname,dob){
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.dob=dob;
+    }
+    
+    getdob(){
+        return this.dob;
+    }
+}  
+
