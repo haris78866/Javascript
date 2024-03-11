76961c9c5b1e5535a714d2b74c4d4342d7dcf40d
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 13:54:31 +0500

    Changes

---
+++ function.js
@@ -0,0 +1,28 @@
+// function in the JS 
+
+function addnum(num1,num2){
+    console.log(num1+num2);
+}
+
+/// calling the function 
+addnum(3,4);
+
+// we can also do with return 
+function addnum(num1,num2){
+    return num1+num2;
+}
+
+console.log(addnum(5,5));
+
+
+// we can alse  intialize the value in the function 
+function addnum(num1=2,num2=1){
+    return num1+num2;
+}
+
+console.log(addnum);
+
+
+// we can also do without return 
+const addnum = (num1 = 1, num2 = 1) => num1 + num2;
+console.log(addnum(4, 4));
