bb9fe7fcc15e4add9ab6049337c4b3f0292d917d
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 17:01:27 +0500

    Changes

---
+++ main2.js
@@ -0,0 +1,15 @@
+console.log(window);
+alert(1);
+
+
+// single element selecter
+
+const form =document.getElementById('conatiner');
+console.log(form);
+
+// we can also grab the conatiner
+const form =document.getElementById('container');
+console.log(form);
+// jquery is JS library 
+
+
