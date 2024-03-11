a7de7f2d9286d678b75f30ce3beada61d54e6494
haris78866 <p230573@pwr.nu.edu.pk>
2024-03-11 13:42:39 +0500

    Changes

---
+++ takingvalue.js
@@ -0,0 +1,9 @@
+// taking value from the user 
+
+//const x=prompt ('Enter the value of x :');
+//console.log(x);
+
+
+
+const userInput =window.confirm ('Enter something :');
+console.log(userInput);
